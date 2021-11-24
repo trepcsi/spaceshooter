668f420fa421e69644a61e570f9b44cb9809efeb
Illes Miklos <illes.miklos95@gmail.com>
2021-11-24 17:28:49 +0100

    first commit

---
+++ html/src/com/trepcsi/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.trepcsi.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.trepcsi.game.SpaceShooter;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SpaceShooter();
+        }
+}
