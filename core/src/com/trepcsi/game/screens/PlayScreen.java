e8681aa0a4a2024e6552bd9c74bd55c853124e78
Illes Miklos <illes.miklos95@gmail.com>
2021-12-04 23:09:55 +0100

    add background

--- core/src/com/trepcsi/game/screens/PlayScreen.java
+++ core/src/com/trepcsi/game/screens/PlayScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
@@ -33,11 +34,14 @@ public class PlayScreen implements Screen {
     private World world;
     private Box2DDebugRenderer box2DDebugRenderer;
 
+    private Texture background;
+
     public PlayScreen(SpaceShooter game) {
         this.game = game;
         camera = new OrthographicCamera();
         viewport = new FitViewport(SpaceShooter.V_WIDTH / SpaceShooter.PPM, SpaceShooter.V_HEIGHT / SpaceShooter.PPM, camera);  //need to scale after b2d
         camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+        background = new Texture("big_background.jpg");
 
         world = new World(new Vector2(0, 0), true);
         world.setContactListener(new WorldContactListener());
@@ -58,11 +62,12 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor((float) 45 / 255, (float) 45 / 255, (float) 180 / 255, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        box2DDebugRenderer.render(world, camera.combined);
-
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
+        game.batch.draw(background, 0, 0, SpaceShooter.V_WIDTH / SpaceShooter.PPM, SpaceShooter.V_HEIGHT / SpaceShooter.PPM);
         game.batch.end();
+
+        box2DDebugRenderer.render(world, camera.combined);
     }
 
     private void update(float dt) {
